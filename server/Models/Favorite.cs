4ee7da084ca10680cb061a3eafe9ba844c675c37
Skyler Kinney <Skylerjkinney@gmail.com>
2024-02-07 16:45:33 -0700

    We got a table BABY

---
+++ server/Models/Favorite.cs
@@ -0,0 +1,8 @@
+namespace AllSpice.Models;
+
+public class Favorites
+{
+    public string Id { get; set; }
+    public string AccountId { get; set; }
+    public int RecipeId { get; set; }
+}
--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,10 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key', createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created', updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update', name varchar(255) COMMENT 'User Name', email varchar(255) COMMENT 'User Email', picture varchar(255) COMMENT 'User Picture'
+) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE recipes (
+    id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(100) NOT NULL, instructions VARCHAR(500) NOT NULL, img VARCHAR(500) NOT NULL, category ENUM(
+        'Cheese', 'Italian', 'Soup', 'Mexican', 'Specialty Coffee'
+    ), creatorId VARCHAR(255) NOT NULL
+    -- FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8mb4 COMMENT '';
